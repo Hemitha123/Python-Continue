09d1e0235ab05f414a85bd50375d63737fd644f2
Hemitha123 <hemithatech@gmail.com>
2025-12-12 09:53:29 +0530

    add changes

---
+++ OPPS/task12.py
@@ -0,0 +1,26 @@
+#Multiple Inheritance 
+class steptracker():
+    def __init__(self,steps):
+        self.steps=steps
+    
+    def calories_burned(self):
+        self.cal=self.steps*0.04
+        
+class sleeptracker():
+    def __init__(self,hours_slept):
+        self.hours_slept=hours_slept
+    def sleep_score(self):
+        self.sc=self.hours_slept*10
+        
+class healthreport(steptracker,sleeptracker):
+    def __init__(self,steps,hours_slept):
+        steptracker.__init__(self,steps)
+        sleeptracker.__init__(self,hours_slept)
+
+    def daily_report(self):
+        print(f"The calories burnt is:{self.cal}")
+        print(f"The sleep score is:{self.sc}")
+h1=healthreport(10000,8)
+h1.calories_burned()
+h1.sleep_score()
+h1.daily_report()
