d47043c92cee970a060ca0fa2b131548fbb0656e
Hemitha123 <hemithatech@gmail.com>
2026-01-06 12:33:54 +0530

    add changes

---
+++ Polymorphism/task1.py
@@ -0,0 +1,13 @@
+class car:
+    def move(self):
+        print("Car drives on road")
+
+class bike:
+    def move(self):
+        print("Bike rides on road")
+Car=car()
+Bike=bike()
+# Car.move()
+# Bike.move()
+for x in (Car,Bike):
+    x.move()
