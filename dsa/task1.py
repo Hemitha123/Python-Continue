6bd75e1852baed9d6fce9da8949dd71cac112b34
Hemitha123 <hemithatech@gmail.com>
2026-02-10 21:10:02 +0530

    add changes

--- dsa/task1.py
+++ dsa/task1.py
@@ -1,17 +1,17 @@
-class stack():
-    def __init__(self):
+class stack():  #class stack
+    def __init__(self):     #initialization of items
         self.items=[]
     def isEmpty(self):
-        return self.items==[]
+        return self.items==[]          #if it is empty then it show true else false
     def push(self,item):
-        self.items.insert(0,item)
+        self.items.insert(0,item)       #adds or inserts the item 
     def pop(self):
-        self.items.pop(0)
+        self.items.pop(0)           #removes only the last item 
     def peek(self):
-        return self.items[0]
+        return self.items[0]        #shows only the first item
     def size(self):
-        return len(self.items)
-S=stack()
+        return len(self.items)      #shows the len of  the stack
+S=stack()       #object called S
 print(S.isEmpty())
 try:
     S.push(2)
@@ -24,7 +24,7 @@ try:
 
     S.pop()
     print(S.items)
-except IndexError:
+except IndexError:      #a condition that raises error statement instead of error in the program
     print("No Item.Pop from empty list")
 try:
     S.peek()
@@ -32,5 +32,5 @@ try:
 except IndexError:
     print("No Item.List index out of range")
 else:
-    print("Done")
+    print("Done")       #if the program runs without an error this statement shows off
 print(len(S.items))
