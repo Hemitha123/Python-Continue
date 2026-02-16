8dda5b41ea4d3566362a06b4ed1ab10d667f22eb
Hemitha123 <hemithatech@gmail.com>
2026-02-16 21:07:20 +0530

    add changes

--- dsa/task2.py
+++ dsa/task2.py
@@ -60,3 +60,36 @@ class LinkedList:
             current = current.next
             position += 1
         return f"Value '{value}' not found in the list"
+llist=LinkedList()
+ch=0
+while ch!=7:
+    print("1.insert data at begining")
+    print("2.insert data at end")
+    print("3.delete data at beginning")
+    print("4.delete data at end")
+    print("5.display data")
+    print("6.search data")
+    print("7.exit")
+    ch=int(input("enter your choice"))
+    if ch==1:
+        a=input("enter the data")
+        llist.insertAtBeginning(a)
+    elif ch==2:
+        b=input("enter the data")
+        llist.insertAtEnd(b)
+    elif ch==3:
+        msg=llist.deleteFromBeginning()
+        if msg:
+            print(msg)
+    elif ch==4:
+        msg=llist.deleteFromEnd()
+        if msg:
+            print(msg)
+    elif ch==5:
+        llist.printList()
+    elif ch==6:
+        f=input("enter the data")
+        print(llist.search(f))
+    else:
+        print("Exit")
+        break
